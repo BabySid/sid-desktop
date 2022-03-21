cb233a1639985d4d0c9ce4513c4c813651a4037e
milimin <milimin@zuoyebang.com>
2022-03-21 20:13:55 +0800

    init favorites

---
+++ desktop/gui/app_favorites.go
@@ -0,0 +1,263 @@
+package gui
+
+import (
+	"fmt"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/binding"
+	"fyne.io/fyne/v2/data/validation"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/widget"
+	"sid-desktop/base"
+	"sid-desktop/desktop/common"
+	"sid-desktop/desktop/storage"
+	sidTheme "sid-desktop/desktop/theme"
+	"strings"
+	"time"
+)
+
+var _ appInterface = (*appFavorites)(nil)
+
+type appFavorites struct {
+	searchEntry  *widget.Entry
+	importFavor  *widget.Button
+	addFavor     *widget.Button
+	exportFavor  *widget.Button
+	favorHeader  *widget.List
+	favorList    *widget.List
+	favorBinding binding.UntypedList
+	favorCache   *common.FavoritesList
+	tabItem      *container.TabItem
+}
+
+func (af *appFavorites) LazyInit() error {
+	err := storage.GetAppFavoritesDB().Open(globalWin.app.Storage().RootURI().Path())
+	if err != nil {
+		return err
+	}
+	base.RegisterAtExit(storage.GetAppFavoritesDB().Close)
+
+	af.searchEntry = widget.NewEntry()
+	af.searchEntry.SetPlaceHolder(sidTheme.AppFavoritesSearchText)
+	af.searchEntry.OnChanged = af.searchFavor
+
+	af.addFavor = widget.NewButtonWithIcon(sidTheme.AppFavoritesAddFavorBtnText, sidTheme.ResourceAddFavorIcon, af.addOneFavor)
+	af.importFavor = widget.NewButtonWithIcon(sidTheme.AppFavoritesImportBtnText, sidTheme.ResourceImportFavorIcon, nil)
+	af.exportFavor = widget.NewButtonWithIcon(sidTheme.AppFavoritesExportBtnText, sidTheme.ResourceExportFavorIcon, nil)
+
+	af.favorBinding = binding.NewUntypedList()
+	af.createFavorList()
+
+	af.tabItem = container.NewTabItemWithIcon(sidTheme.AppFavoritesName, sidTheme.ResourceFavoritesIcon, nil)
+	af.tabItem.Content = container.NewBorder(
+		container.NewGridWithColumns(2,
+			af.searchEntry,
+			container.NewHBox(layout.NewSpacer(), af.importFavor, af.exportFavor, af.addFavor)), nil, nil, nil,
+		container.NewBorder(af.favorHeader, nil, nil, nil, af.favorList),
+	)
+
+	go af.initDB()
+
+	return nil
+}
+
+func (af *appFavorites) GetTabItem() *container.TabItem {
+	return af.tabItem
+}
+
+func (af *appFavorites) GetAppName() string {
+	return sidTheme.AppFavoritesName
+}
+
+func (af *appFavorites) OpenDefault() bool {
+	return false
+}
+
+func (af *appFavorites) OnClose() bool {
+	return true
+}
+
+func (af *appFavorites) createFavorList() {
+	// Favor List Header
+	af.favorHeader = widget.NewList(
+		func() int {
+			return 1
+		},
+		func() fyne.CanvasObject {
+			return container.NewGridWithColumns(3,
+				widget.NewLabelWithStyle("", fyne.TextAlignLeading, fyne.TextStyle{}),
+				widget.NewLabelWithStyle("", fyne.TextAlignCenter, fyne.TextStyle{}),
+				widget.NewLabelWithStyle("", fyne.TextAlignTrailing, fyne.TextStyle{}))
+		},
+		func(id widget.ListItemID, item fyne.CanvasObject) {
+			item.(*fyne.Container).Objects[0].(*widget.Label).SetText(sidTheme.AppFavoritesFavorListHeader1)
+			item.(*fyne.Container).Objects[1].(*widget.Label).SetText(sidTheme.AppFavoritesFavorListHeader2)
+			item.(*fyne.Container).Objects[2].(*widget.Label).SetText("")
+		},
+	)
+
+	// Favor Data
+	af.favorList = widget.NewListWithData(
+		af.favorBinding,
+		func() fyne.CanvasObject {
+			return container.NewGridWithColumns(3,
+				widget.NewLabelWithStyle("", fyne.TextAlignLeading, fyne.TextStyle{}),
+				widget.NewLabelWithStyle("", fyne.TextAlignCenter, fyne.TextStyle{}),
+				container.NewHBox(
+					layout.NewSpacer(),
+					widget.NewButtonWithIcon(sidTheme.AppFavoritesFavorListOp1, sidTheme.ResourceEditFavorIcon, nil),
+					widget.NewButtonWithIcon(sidTheme.AppFavoritesFavorListOp2, sidTheme.ResourceOpenFavorIcon, nil)),
+			)
+		},
+		func(data binding.DataItem, item fyne.CanvasObject) {
+			o, _ := data.(binding.Untyped).Get()
+			favor := o.(common.Favorites)
+
+			name := favor.Name + "(" + strings.Join(favor.Tags, common.FavorTagSep) + ")"
+			item.(*fyne.Container).Objects[0].(*widget.Label).SetText(name)
+			item.(*fyne.Container).Objects[1].(*widget.Label).SetText(favor.Url)
+
+			item.(*fyne.Container).Objects[2].(*fyne.Container).Objects[1].(*widget.Button).SetText(sidTheme.AppFavoritesFavorListOp1)
+			item.(*fyne.Container).Objects[2].(*fyne.Container).Objects[1].(*widget.Button).OnTapped = func() {
+				_ = storage.GetAppFavoritesDB().RmFavorites(favor)
+				af.reloadFavorList()
+			}
+			item.(*fyne.Container).Objects[2].(*fyne.Container).Objects[2].(*widget.Button).SetText(sidTheme.AppFavoritesFavorListOp2)
+			item.(*fyne.Container).Objects[2].(*fyne.Container).Objects[2].(*widget.Button).OnTapped = func() {
+				err := globalWin.app.OpenURL(common.ParseURL(favor.Url))
+				if err != nil {
+					printErr(fmt.Errorf(sidTheme.OpenFavoritesFailedFormat, err))
+				}
+			}
+		},
+	)
+}
+
+func (af *appFavorites) searchFavor(name string) {
+	if name == "" {
+		// show all favorites
+		if af.favorCache != nil {
+			_ = af.favorBinding.Set(af.favorCache.AsInterfaceArray())
+		}
+	} else {
+		if af.favorCache != nil {
+			rs := af.favorCache.Find(name)
+			_ = af.favorBinding.Set(rs.AsInterfaceArray())
+		}
+	}
+}
+
+func (af *appFavorites) addOneFavor() {
+	name := widget.NewEntry()
+	name.Validator = validation.NewRegexp(`\S+`, sidTheme.AppFavoritesAddFavorName+" must not be empty")
+	url := widget.NewEntry()
+	url.SetText("https://")
+	url.Validator = validation.NewRegexp(`\S+`, sidTheme.AppFavoritesAddFavorUrl+" must not be empty")
+
+	tags := widget.NewEntry()
+	expand := widget.NewButtonWithIcon(sidTheme.AppFavoritesAddFavorExpand, sidTheme.ResourceExpandDownIcon, nil)
+
+	tagArray := binding.NewStringList()
+	tagList := widget.NewListWithData(
+		tagArray,
+		func() fyne.CanvasObject {
+			return widget.NewLabel("")
+		},
+		func(item binding.DataItem, o fyne.CanvasObject) {
+			o.(*widget.Label).Bind(item.(binding.String))
+		},
+	)
+	tagList.Hide()
+	cont := container.NewBorder(
+		container.NewVBox(
+			widget.NewForm(
+				widget.NewFormItem(sidTheme.AppFavoritesAddFavorUrl, url),
+				widget.NewFormItem(sidTheme.AppFavoritesAddFavorName, name),
+				widget.NewFormItem(sidTheme.AppFavoritesAddFavorTags, tags),
+			),
+			container.NewHBox(expand, layout.NewSpacer()),
+		),
+		nil, nil, nil,
+		tagList,
+	)
+
+	tags.OnChanged = func(s string) {
+		arr := strings.Split(s, common.FavorTagSep)
+		_ = tagArray.Set(arr)
+	}
+
+	win := dialog.NewCustomConfirm(
+		sidTheme.AppFavoritesAddFavorTitle, sidTheme.ConfirmText, sidTheme.DismissText,
+		cont, func(b bool) {
+			if b {
+				t, _ := tagArray.Get()
+				favor := common.Favorites{
+					Name:       name.Text,
+					Url:        url.Text,
+					Tags:       t,
+					CreateTime: time.Now().Unix(),
+				}
+				err := storage.GetAppFavoritesDB().AddFavorites(favor)
+				if err != nil {
+					printErr(fmt.Errorf(sidTheme.OpenFavoritesFailedFormat, err))
+				}
+
+				af.reloadFavorList()
+			}
+		},
+		globalWin.win)
+
+	expand.OnTapped = func() {
+		if tagList.Visible() {
+			tagList.Hide()
+			win.Resize(fyne.NewSize(500, 300))
+
+			expand.SetText(sidTheme.AppFavoritesAddFavorExpand)
+			expand.SetIcon(sidTheme.ResourceExpandDownIcon)
+		} else {
+			tagList.Show()
+			win.Resize(fyne.NewSize(500, 500))
+
+			expand.SetText(sidTheme.AppFavoritesAddFavorShrink)
+			expand.SetIcon(sidTheme.ResourceExpandUpIcon)
+		}
+	}
+
+	win.Resize(fyne.NewSize(500, 300))
+
+	win.Show()
+}
+
+func (af *appFavorites) reloadFavorList() {
+	af.loadFavoritesFromDB()
+	if af.favorCache != nil {
+		_ = af.favorBinding.Set(af.favorCache.AsInterfaceArray())
+	}
+}
+
+func (af *appFavorites) loadFavoritesFromDB() {
+	var err error
+	af.favorCache, err = storage.GetAppFavoritesDB().LoadFavorites()
+	if err != nil {
+		printErr(fmt.Errorf(sidTheme.OpenFavoritesFailedFormat, err))
+	}
+}
+
+func (af *appFavorites) initDB() {
+	need, err := storage.GetAppFavoritesDB().NeedInit()
+	if err != nil {
+		printErr(fmt.Errorf(sidTheme.OpenFavoritesFailedFormat, err))
+		return
+	}
+
+	if need {
+		err = storage.GetAppFavoritesDB().Init()
+		if err != nil {
+			printErr(fmt.Errorf(sidTheme.OpenFavoritesFailedFormat, err))
+			return
+		}
+	} else {
+		af.reloadFavorList()
+	}
+}
